bf2cebc24e15f433d9a04f8b49941361eff9590d
Glen Maddern <glenmaddern@gmail.com>
2012-06-04 18:00:04 +1000

    coffeescript serving

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'coffee-script'
+require 'haml'
 
 set :haml, format: :html5
 set :views, '.'
 
 get '/' do
   haml :index
+end
+
+get '/application.js' do
+  coffee :application
 end
