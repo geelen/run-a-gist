6dbe42b2716d184f59b343e65dedf1dbabe4ddad
Glen Maddern <glenmaddern@gmail.com>
2012-06-06 09:22:31 +1000

    got this working (almost!)

--- Gemfile
+++ Gemfile
@@ -1,8 +1,7 @@
 source :rubygems
 gem 'thin'
 gem 'sinatra'
-gem 'sinatra-contrib'
 gem 'haml'
 gem 'coffee-script'
-gem 'pry'
 gem 'curb'
+gem 'json'
--- server.rb
+++ server.rb
@@ -1,29 +1,53 @@
 require 'sinatra'
-require 'sinatra/reloader' if development?
 require 'coffee-script'
 require 'haml'
 require 'curb'
+require 'json'
 
 set :haml, format: :html5
-set :views, 'app'
 
 def gist_id
-  request.host[/^(\w+)\./,1]
+  @gist_id ||= request.host[/^(\w+)\./, 1]
 end
 
-def gist(filename)
-  Curl::Easy.perform("http://gist.github.com/raw/#{gist_id}/#{filename}") { |e|
-    e.follow_location = true
-  }.body_str
+def fetch(url)
+  Curl::Easy.perform(url) { |e| e.follow_location = true }.body_str
 end
 
-get '/application.js' do
-  coffee gist('application.coffee')
+def get_raw(filename)
+  fetch("http://gist.github.com/raw/#{gist_id}/#{filename}")
 end
 
-get '/favicon.ico' do
+def manifest
+  @manifest ||= JSON.parse(fetch("http://gist.github.com/api/v1/json/#{gist_id}"))
 end
 
-get '*' do
-  haml gist('index.haml')
+def files
+  @files ||= manifest.fetch('gists').map { |g| g.fetch('files') }.flatten
+rescue KeyError
+  []
+end
+
+HANDLERS = {
+  js: {js: -> js { js }, coffee: -> js { CoffeeScript.compile js } },
+  html: {html: -> html { html }, haml: -> html { Haml::Engine.new(html).render }}
+}
+
+get 'wat' do
+
+end
+
+get '/*' do
+  if gist_id
+    input = params[:captures].first.empty? ? "index.html" : params[:captures].first
+    extension,name = File.basename(input).reverse.split(/\./,2).map(&:reverse)
+    (HANDLERS[extension.to_sym] || []).each do |source_ext, handler|
+      if files.include? "#{name}.#{source_ext}"
+        content_type extension
+        return handler[get_raw("#{name}.#{source_ext}")]
+      end
+    end
+  else
+    haml :no_gist_id
+  end
 end
